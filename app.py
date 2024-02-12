59540d2b1c4dc2e7310f80e8cf9265a744a7950d
Djegui-lab <dwague44@gmail.com>
2024-02-12 20:19:54 +0100

    oo

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.application import MIMEApplication
 from datetime import datetime
 import google 
-import google_auth
+
 from google.oauth2 import service_account
 from google.oauth2.service_account import Credentials
 import gspread
