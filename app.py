244b9fe7c46e0121d4ce9d8033f1232cfa270728
Djegui-lab <dwague44@gmail.com>
2024-02-13 02:38:28 +0100

    ok

--- app.py
+++ app.py
@@ -56,10 +56,10 @@ st.markdown(
 
 # Fonction pour envoyer un e-mail avec la nouvelle signature HTML dans le corps
 def envoyer_email(nom, prenom, email, piece_jointe=None, documents_recus=False):
-    smtp_server = os.getenv("SMTP_SERVER")
-    port = os.getenv("PORT")  # Port sécurisé SSL pour Gmail
-    adresse_expediteur = os.getenv("ADRESSE_EXPEDITEUR")  # Remplacez par votre adresse e-mail Gmail
-    mot_de_passe = os.getenv("MOT_DE_PASSE")  # Remplacez par votre mot de passe Gmail
+    smtp_server ="smtp.gmail.com"
+    port = "465" # Port sécurisé SSL pour Gmail
+    adresse_expediteur = "dwague44@gmail.com" # Remplacez par votre adresse e-mail Gmail
+    mot_de_passe = "ahdxhsqsmsacafjv"  # Remplacez par votre mot de passe Gmail
 
     sujet = f"Votre demande de devis pour l'assurance auto ! {nom}"
     corps = f"""<span style="color: black;">
